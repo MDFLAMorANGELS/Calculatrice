0929686c13271427c408d32a1ea611e75a3b35e1
MDFLAMorANGELS <valentinmd78@gmail.com>
2024-08-28 23:06:35 +0200

    Creaion calculatrice

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ function App() {
 
   return (
     <>
-      <div className='container'>
-       
+      <div className='bgCalculatrice bg-green-400 w-[500px] h-[800px] rounded-[40px] border-2 py-3 px-5 border-black flex items-center flex-col relative'>
+        <h1 className=" text-black text-xl text-left absolute left-0 ml-5">Fx Mdflamorangels</h1>
+        <div className="bg-slate-500 h-[100px] w-full mt-8 flex justify-center items-center rounded">
+
+        </div>
+        <div className="sectionButton flex flex-wrap">
+          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
+          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
+          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
+          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
+          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
+          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
+          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
+          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
+          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
+        </div>
       </div>
     </>
   )
