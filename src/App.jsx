df350e1d04ce2650111bf7dd9c5f4f55ab368986
MDFLAMorANGELS <valentinmd78@gmail.com>
2024-08-29 19:18:56 +0200

    Ajout des button avec un map de l array + commencement du screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
+import { useState } from "react";
 
 function App() {
+  
+  const [screenValue, setScreenValue] = useState(0)
+
+  const buttonValues = [
+    "%", "CE", "C", "X", 
+    "7", "8", "9", "/", 
+    "4", "5", "6", "-", 
+    "1", "2", "3", "+", 
+    "+/-", "0", ",", "="
+  ];
+
+  function handle(value) {
+    setScreenValue(value)
+  }
+  
+  const buttonClasss = "bg-slate-200 hover:bg-slate-300 text-black font-bold text-lg py-[6px] px-4 border-b-4 hover:border-b-2 border-x-2 border-t-2 border-slate-500 hover:border-black rounded transition-all h-12"
 
   return (
     <>
-      <div className='bgCalculatrice bg-green-400 w-[500px] h-[800px] rounded-[40px] border-2 py-3 px-5 border-black flex items-center flex-col relative'>
+      <div className='bgCalculatrice bg-green-400 w-[430px] h-[600px] rounded-[40px] border-2 py-3 px-5 border-black flex items-center flex-col relative'>
         <h1 className=" text-black text-xl text-left absolute left-0 ml-5">Fx Mdflamorangels</h1>
-        <div className="bg-slate-500 h-[100px] w-full mt-8 flex justify-center items-center rounded">
-
+        <div className="bg-slate-500 h-[80px] w-full mt-8 flex justify-center items-center rounded">
+          <input type="text" className="w-full h-full bg-transparent text-right text-black font-bold px-2 text-5xl" readOnly disabled value={screenValue} />
         </div>
-        <div className="sectionButton flex flex-wrap">
-          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
-          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
-          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
-          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
-          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
-          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
-          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
-          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
-          <button className="button rounded bg-white m-5 text-black w-20 h-10 border-2 border-black">1</button>
+        <div className="sectionButton grid grid-cols-4 gap-8 mt-8">
+          {buttonValues.map((value,index) => (
+            <button key={index} className={buttonClasss} onClick={() => handle(value)}>
+              {value}
+            </button>
+          ))}
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
